3a7600fdc1b0661aeeb68edb0caf382e8eb979e4
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-01-05 02:39:09 +0500

    im fuckd up again lol

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const Bot = require('node-telegram-bot-api');
 const TOKEN = '510955923:AAESsAwYGz2OxfIVApuNVYop89HkdD8ygbc';
-const bot = new Bot(token, { polling: true });
+const url = process.env.APP_URL || 'https://my-test-bot-10950.herokuapp.com:443';
 
-bot.on('message', message => {
-  bot.sendMessage(message.chat.id, `hello, ${message.from.first_name}!`);
+const options = {
+  webHook: {
+    port: process.env.port
+  }
+};
+
+const bot = new Bot(TOKEN, options);
+bot.setWebHook(`${url}/bot${TOKEN}`);
+
+bot.on('message', function onMessage(msg) {
+  bot.sendMessage(msg.chat.id, 'I am alive on Heroku!');
 });

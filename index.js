36268cca7ee8b8c22297a16f6516004a13b2d5bc
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-01-07 06:48:12 +0500

    extend answers

--- index.js
+++ index.js
@@ -25,14 +25,33 @@ bot.setWebHook(`${url}/bot${TOKEN}`);
 const normalizeString = (string) => string.toLowerCase().trim();
 
 
+const daneel = [
+  'Ты налил?',
+  'Ты галил',
+  'Ты запилил?',
+  'Ты залил?',
+  'Ты заплатил?',
+  'Ты разбил?',
+  'Ты изучил?',
+  'Ты установил?',
+  'Ты поразил',
+  'Ты сходил?',
+  'Ты утвердил?',
+  'Ты победил?',
+  'Ты подтвердил?'
+];
+
+
 // Just to ping!
 bot.on('message', function onMessage(msg) {
   if (msg.text) {
     const text = normalizeString(msg.text);
 
     switch (text) {
+      case '/pidor':
+        bot.sendMessage(msg.chat.id, `Опять в свои игрульки играете? Пиздец, вам сколько уже? 24? 25? У нормальных людей уже семья, дети, а вы все генератором псевдорандомных чисел решаете кто из вас пидор в чате, охуеть можно. ${msg.from.first_name}, повзрослей, блядь.`);
       case 'данил':
-        bot.sendMessage(msg.chat.id, 'Ты налил?');
+        bot.sendMessage(msg.chat.id, items[Math.floor(Math.random() * daneel.length)]);
         break;
       case 'здесь так красиво':
         bot.sendMessage(msg.chat.id, 'Я перестаю дышать');
@@ -40,11 +59,26 @@ bot.on('message', function onMessage(msg) {
       case 'звуки на минимум':
         bot.sendMessage(msg.chat.id, 'Чтобы не мешать');
         break;
+      case 'эти облака':
+        bot.sendMessage(msg.chat.id, 'Фиолетовая ватааааа');
+        break;
+      case 'магия цветов':
+        bot.sendMessage(msg.chat.id, 'со льдом в наших стаканааах');
+        break;
       case 'все говорят ака':
         bot.sendMessage(msg.chat.id, 'а как поднять бабла?');
         break;
       case 'все говорят рушан':
         bot.sendMessage(msg.chat.id, 'ты че такой еблан?');
+        break;
+      case 'все говорят кирилл':
+        bot.sendMessage(msg.chat.id, 'будь как данил');
+        break;
+      case 'все говорят миша':
+        bot.sendMessage(msg.chat.id, 'некрасовский коп, оп, коп');
+        break;
+      case 'все говорят касарь':
+        bot.sendMessage(msg.chat.id, 'ты че такой букварь?');
         break;
       case 'все говорят михрон':
         bot.sendMessage(msg.chat.id, 'ты че такой гандон');
@@ -55,6 +89,13 @@ bot.on('message', function onMessage(msg) {
         break;
       case 'все говорят айдар':
         bot.sendMessage(msg.chat.id, `зачем ты ебал ${msg.from.first_name} вчера?`);
+        break;
+      case 'бот подтвердит':
+        if (msg.from.first_name === 'Amma') {
+          bot.sendMessage(msg.chat.id, `Плюсану к Айди`);
+        } else {
+          bot.sendMessage(msg.chat.id, `Ну хуй знает`);
+        }
         break;
     }
   }
